7490c40aa93031e5765b8c02ca95e642348980ee
Ryan Cummings <rcummings98@gmail.com>
2024-04-07 14:00:44 -0600

    kjsdhkjsjksdkj

--- CourseProject/programs/dollar_normalization.py
+++ CourseProject/programs/dollar_normalization.py
@@ -4,57 +4,55 @@ import numpy as np
 
 def normalize_revenue_data(revenue_path):
     raw_data = pd.read_csv(revenue_path, sep=',')
-#    print(raw_data.to_string)
 
 
-def calculate_quartly_cpi(cpi_path):
-    """averages the monthly CPILEGNS using arithmetic mean
+def derive_quarterly_cpi(cpi_path):
 
-    Parameters
-    ----------
-    cpi_path : str
-    path to CPI data csv file
-
-    Returns
-    -------
-    quarterly_cpi : Pandas DataFrame
-        new dataframe that reorganizes the cpi from monthly to quarterly
-    """
-
-    start_date = '2009-03-30'
-    end_date = '2023-10-01'
-
-    # read in csv file and process into dataframe
+    # Read csv file into pandas dataframe
     monthly_cpi = pd.read_csv(cpi_path)
-    monthly_cpi = monthly_cpi.set_index('DATE')
 
-    # remove unneeded data by date range
-    # the boolean mask is a good idea from Gemini (google's chatbot)
-    mask = (monthly_cpi['DATE'] >= start_date & monthly_cpi['DATE'] <= end_date)
-    filtered_cpi = monthly_cpi[mask]
+    # Filter by date range
+    monthly_cpi['DATE'] = pd.to_datetime(monthly_cpi['DATE'])
+    monthly_cpi.set_index('DATE')
+    monthly_cpi = monthly_cpi.loc[(monthly_cpi['DATE'] >= '2009-1-1') & (monthly_cpi['DATE'] <= '2023-9-30')]
+    monthly_cpi.reset_index(drop=True, inplace=True)
 
-    # Dates are formatted as strings and must be converted to
-    # datetime objects so pandas can work with them
-    filtered_cpi.index = pd.to_datetime(monthly_cpi.index)
+    # Transform to quarterly CPI using arithmetic mean
+    quarterly_cpi = monthly_cpi.set_index('DATE').resample('Q').mean().round(1)
 
-    # resample() converts monthly to quarterly data using arithmetic mean
-    quarterly_cpi = monthly_cpi.resample('Q')['CPILEGNS'].mean()
-
-    # reset_index makes dataframe easy for pandas to mutate
-    quarterly_cpi = quarterly_cpi.reset_index()
-
-    print("quarterly_cpi")
-    print(quarterly_cpi.to_string)
-    quarterly_cpi.to_csv('quarterly_cpi.csv')
     return quarterly_cpi
+
+
+def normalize_revenue_data(revenue_path, cpi):
+    # read revenue data csv and format as pandas dataframe
+    nominal_data = pd.read_csv(revenue_path)
+    # print(nominal_data)
+
+    # Do some formatting to make it easier to work with
+    nominal_data = nominal_data.rename(columns={"AWR Date": "awr_date",
+                                 "AWR Quarterly Revenue (Millions USD)": "awr_nom",
+                                 "RRGB Date": "rrgb_date",
+                                 "RRGB Quarterly Revenue (Millions USD)": "rrbg_nom"})
+    calculate_real_value(nominal_data, cpi)
+
+    # Add real value column and insert normalized dollar values
+    return nominal_data
+
+def calculate_real_value(data, cpi):
+    data['cpi'] = cpi['CPILEGNS'].values
+    data['awr_nom'] = data['awr_nom'].str.replace('$', '')
+    data['numeric_dollars'] = pd.to_numeric(data['awr_nom'])
+    data['awr_real'] = data.eval('numeric_dollars / cpi')
+    print(data)
+
 
 
 def main():
     cpi_path = "CPILEGNS.csv"
     revenue_path = "raw_data.csv"
 
-    quarterly_cpi = calculate_quartly_cpi(cpi_path)
-    normalized_data = normalize_revenue_data(revenue_path)
+    quarterly_cpi = derive_quarterly_cpi(cpi_path)
+    real_dollar_data = normalize_revenue_data(revenue_path, quarterly_cpi)
 
 
 main()
